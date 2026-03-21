83de5e15d44bae17209b49910cb7cb107e0415de
Emmanuel Bigot <bigotb9@gmail.com>
2026-03-21 16:55:17 +0000

    fix layout server component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,6 @@
-"use client"
-
 import "./globals.css"
 import Sidebar from "@/components/Sidebar"
 import AuthGuard from "@/components/AuthGuard"
-import { usePathname } from "next/navigation"
 import { ThemeProvider } from "next-themes"
 
 export default function RootLayout({
@@ -12,24 +9,18 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
 
-  const pathname = usePathname()
-  const isLoginPage = pathname === "/"
-
   return (
     <html lang="fr" suppressHydrationWarning>
       <body className="bg-background text-foreground">
         <ThemeProvider attribute="class" defaultTheme="dark">
 
-          {isLoginPage ? (
-            children
-          ) : (
-            <AuthGuard>
-              <Sidebar />
-              <main className="ml-64 p-6 bg-gray-100 dark:bg-[#0B0B0F] min-h-screen">
-                {children}
-              </main>
-            </AuthGuard>
-          )}
+          {/* ⚠️ on ne met PLUS de logique ici */}
+          <AuthGuard>
+            <Sidebar />
+            <main className="ml-64 p-6 bg-gray-100 dark:bg-[#0B0B0F] min-h-screen">
+              {children}
+            </main>
+          </AuthGuard>
 
         </ThemeProvider>
       </body>
